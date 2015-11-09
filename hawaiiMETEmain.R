c78f72decdf8f7b641a2f363bdd97c73374db9ac
ajrominger <ajrominger@gmail.com>
2015-11-08 23:26:38 -0700

    prettified plotting by making lims consistent and adding margin text to designate trophic position and site

--- hawaiiMETEmain.R
+++ hawaiiMETEmain.R
@@ -30,17 +30,21 @@ byTrophBySite <- cbind(byTrophBySite,
 
 
 ## plot SAD
-pdf(file='fig_allSAD.pdf', width=5, height=5)
-par(mar=rep(0.1, 4), oma=c(4, 4, 0, 0)+0.1)
+pdf(file='fig_allSAD.pdf', width=6, height=4)
+
+par(mar=rep(0.1, 4), oma=c(4, 4, 2, 2)+0.1)
 layout(matrix(1:(3*5), nrow=3, byrow=TRUE))
 
 for(troph in c('D', 'H', 'P')) {
     for(site in c('VO', 'LA', 'KH', 'MO', 'KA')) {
         plot(mete.byTS[[which(byTrophBySite$trophic==troph & 
                                   byTrophBySite$Site==site)]]$sad, 
-             ptype='rad', log='y', xaxt='n', yaxt='n', add.legend=FALSE,
+             ptype='rad', add.legend=FALSE,
+             log='y', yaxt=ifelse(site=='VO', 's', 'n'), xaxt=ifelse(troph=='P', 's', 'n'), 
              xlim=c(1, max(byTrophBySite$S0)), ylim=c(1, max(byTrophBySite$maxN)))
-        legend('topright', legend=paste(site, troph))
+        
+        if(site=='KA') mtext(troph, side=4, line=1)
+        if(troph=='D') mtext(site, side=3, line=1)
     }
 }
 
@@ -48,16 +52,21 @@ dev.off()
 
 
 ## plot IPD
-pdf(file='fig_allIPD.pdf', width=5, height=5)
-par(mar=rep(0.1, 4), oma=c(4, 4, 0, 0)+0.1)
+pdf(file='fig_allIPD.pdf', width=6, height=4)
+
+par(mar=rep(0.1, 4), oma=c(4, 4, 2, 2)+0.1)
 layout(matrix(1:(3*5), nrow=3, byrow=TRUE))
 
 for(troph in c('D', 'H', 'P')) {
     for(site in c('VO', 'LA', 'KH', 'MO', 'KA')) {
         plot(mete.byTS[[which(byTrophBySite$trophic==troph & 
                                   byTrophBySite$Site==site)]]$ipd, 
-             ptype='rad', log='y', xaxt='n', yaxt='n', add.legend=FALSE)
-        legend('topright', legend=paste(site, troph))
+             ptype='rad', add.legend=FALSE,
+             log='y', yaxt=ifelse(site=='VO', 's', 'n'), xaxt=ifelse(troph=='P', 's', 'n'), 
+             xlim=c(1, max(byTrophBySite$S0)), ylim=c(1, max(byTrophBySite$maxN)))
+        
+        if(site=='KA') mtext(troph, side=4, line=1)
+        if(troph=='D') mtext(site, side=3, line=1)
     }
 }
 
