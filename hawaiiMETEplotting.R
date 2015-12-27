5a3991938ad1b086fa9bc5a8e272155f7a579f4d
ajrominger <ajrominger@gmail.com>
2015-12-27 13:16:15 -0700

    added proportion of N, S, E coopted by non-natives

--- hawaiiMETEplotting.R
+++ hawaiiMETEplotting.R
@@ -11,22 +11,35 @@ site.info <- read.csv('~/Research/data/Gruner/guner_site.csv')
 load('mete.byTS.RData')
 source('~/R_functions/logAxis.R')
 
+
+
+## ===========================================
+## organize all info into byTrophBySite
+## ===========================================
+
+## state vars
 byTrophBySite <- cbind(byTrophBySite,
                        t(sapply(mete.byTS, function(x) {
                            c(x$sad$state.var, maxN=max(x$sad$data), maxE=max(x$ipd$data))
                        })))
 
+## ages
 byTrophBySite$siteAge <- site.info$age[match(byTrophBySite$Site, site.info$code)]
 byTrophBySite$islandAge <- site.info$island.age[match(byTrophBySite$Site, site.info$code)]
 
-apply(byTrophBySite[, 1:2], 1, function(m) {
-    this.match <- x[paste(m['trophic'], m['Site']) == paste(x$trophic, x$Site), drop=FALSE]
-    Nnon <- sum((this.match$Origin != 'endemic' | this.match$Origin != 'indigenous')) / nrow(this.match)
-    Snon <- length(unique(this.match$SpeciesCode[this.match$Origin != 'endemic' | this.match$Origin != 'indigenous'])) / 
+## proportion non-native
+byTrophBySite <- cbind(byTrophBySite, t(apply(byTrophBySite[, 1:2], 1, function(m) {
+    this.match <- x[paste(m['trophic'], m['Site']) == paste(x$trophic, x$Site), , drop=FALSE]
+    Nnon <- 1 - sum((this.match$Origin == 'endemic' | this.match$Origin == 'indigenous')) / nrow(this.match)
+    Snon <- 1 - length(unique(this.match$SpeciesCode[this.match$Origin == 'endemic' | this.match$Origin == 'indigenous'])) / 
         length(unique(this.match$SpeciesCode))
-    ## do E then be done
-})
+    Enon <- 1 - sum(this.match$IND_BIOM[this.match$Origin == 'endemic' | this.match$Origin == 'indigenous']^0.75) /
+        sum(this.match$IND_BIOM^0.75)
+    
+    return(c(Nnon, Snon, Enon))
+})))
 
+names(byTrophBySite)[10:12] <- c('N.none', 'S.none', 'E.none')
 
 ## ===========================================
 ## plot SAD
