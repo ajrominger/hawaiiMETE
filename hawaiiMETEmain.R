ed126e1c7abfa9d18d90512f004e7c7e295a5ca5
ajrominger <ajrominger@gmail.com>
2015-12-31 14:53:48 -0700

    fixed by site by troph by tree to actually subset data by tree. added conditional statement skipping mete analysis on samples with <= 50 individuals

--- hawaiiMETEmain.R
+++ hawaiiMETEmain.R
@@ -14,6 +14,10 @@ byTrophBySite <- byTrophBySite[!(byTrophBySite$trophic %in% c('U', 'T')), ]
 byTrophBySiteByTree <- unique(x[, c('trophic','Site', 'Tree')])
 byTrophBySiteByTree <- byTrophBySiteByTree[!(byTrophBySiteByTree$trophic %in% c('U', 'T')), ]
 
+bySite <- unique(x$Site)
+
+bySiteByTree <- unique(x[, c('Site', 'Tree')])
+
 ## mete objects divided by trophic and site
 mete.byTS <- apply(byTrophBySite, 1, function(s) {
     dat <- x[x$trophic==s[1] & x$Site==s[2], ]
@@ -24,9 +28,14 @@ mete.byTS <- apply(byTrophBySite, 1, function(s) {
 
 ## mete objects divided by trophic, site and tree
 mete.byTST <- apply(byTrophBySiteByTree, 1, function(s) {
-    dat <- x[x$trophic==s[1] & x$Site==s[2], ]
-    esf <- meteESF(dat$SpeciesCode, dat$Abundance, dat$IND_BIOM^0.75)
-    out <- list(sad=sad(esf), ipd=ipd(esf))
+    dat <- x[x$trophic==s[1] & x$Site==s[2] & x$Tree==as.numeric(s[3]), ]
+    this.n <- sum(dat$Abundance)
+    if(sum(dat$Abundance) > 50) {
+        esf <- meteESF(dat$SpeciesCode, dat$Abundance, dat$IND_BIOM^0.75)
+        out <- list(sad=sad(esf), ipd=ipd(esf))  
+    } else {
+        out <- NULL
+    }  
     return(out)
 })
 
