3fbca62c2732dd7ad1ad765df1551bf864bdcc79
ajrominger <ajrominger@gmail.com>
2016-09-06 10:48:57 -0700

    made figure for john

---
+++ psi4john.R
@@ -0,0 +1,11 @@
+library(meteR)
+library(socorro)
+load('mete.byTS.RData')
+
+pdf('fig_psi4john.pdf', width = 5, height = 5)
+par(mar = c(3, 3, 2, 0) + 0.1, mgp = c(2, 0.75, 0))
+i <- 6
+plot(mete.byTS[[i]]$ipd, ptype = 'rad', log = 'y', yaxt = 'n')
+logAxis(2)
+mtext('Kohala predatory arthropods', line = 1)
+dev.off()
