09f986aef0af82b3fe5471331e19aa53dca61615
ajrominger <ajrominger@gmail.com>
2016-10-20 12:48:23 -0700

    cleaned up plotting script

--- hawaiiMETE_plotting.R
+++ hawaiiMETE_plotting.R
@@ -6,9 +6,6 @@ load('mete.byTS.RData')
 
 with(byTrophBySite[byTrophBySite$trophic == 'H', ], 
      plot(factor(as.character(Site), levels = c('VO', 'LA', 'KH', 'MO', 'KA')), 
-          S0))
-
-
-with(byTrophBySite[byTrophBySite$trophic == 'H', ], plot(S0, sad.z, xlim = range(byTrophBySite$S0)))
+          sad.z))
 
 plot(byTrophBySite$S0, byTrophBySite$sad.z, col = factor(byTrophBySite$trophic))
